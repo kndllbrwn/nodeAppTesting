b011d80a41df0375065d5868782e070a215516c2
kndllbrwn <kndllbrwn1914@gmail.com>
2017-01-23 18:16:46 -0500

    Did more testing using assertions using Expect NPM

--- utils/utils.js
+++ utils/utils.js
@@ -4,3 +4,10 @@ module.exports.add = (a,b) => a + b; /*following lines show older less efficient
  };*/
 
  module.exports.square = x => x * x;
+
+ module.exports.setName = (user, fullName) => {
+     var names = fullName.split(" ");
+     user.firstName = names[0];
+     user.lastName = names[1];
+     return user; 
+ };
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,41 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
+    
     var res = utils.add(33,11);
-    if (res !== 44){
-        throw new Error(`Expected 44, but got ${res}`)
-    }
+    expect(res).toBe(44).toBeA('number');
+    // if (res !== 44){
+    //     throw new Error(`Expected 44, but got ${res}`)
+    // }
 });
 
 it('should square a number', () => {
     var res = utils.square(2);
-    if (res !== 4){
-        throw new Error(`Expected 4, but got ${res}`)
-    }
+    expect(res).toBe(4).toBeA('number');
+    // {
+    //     throw new Error(`Expected 4, but got ${res}`)
+    // }
 });
+
+it('should verify first and last names are set', () => {
+    var res = utils.setName({age:29, occupation:"Developer"}, "Ken Brown");
+    expect(res).toInclude({
+        firstName: "Ken",
+        lastName: "Brown"
+    });
+})
+
+// it('should expect some values', () => {
+//     // expect(12).toNotBe(11);
+//     // expect({name: "Ken"}).toNotEqual({name:"ken"});
+//     // expect([2,3,4]).toExclude(1);
+
+//     expect({
+//         name: "Ken",
+//         age: 29,
+//         location: "DC"
+//     }).toExclude({
+//         age: 23
+//     })
+// });
